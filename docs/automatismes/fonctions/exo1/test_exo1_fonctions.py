65255402b8c7048bd1f05221472af2050da382a3
Frédéric Junier <admin@frederic-junier.org>
2021-06-07 18:06:19 +0200

    maj

--- docs/automatismes/fonctions/exo1/test_exo1_fonctions.py
+++ docs/automatismes/fonctions/exo1/test_exo1_fonctions.py
@@ -1,6 +1,4 @@
 import io
-import js
-
 sortie = io.StringIO() 
 benchmark = ['somme([]) == None', 'somme([1]) == 1', 'somme([1,2]) == 3', 'somme([-1,1]) == 0']
 for k, test in enumerate(benchmark, 1):
@@ -10,45 +8,5 @@ for k, test in enumerate(benchmark, 1):
         sortie.write(f'Test {k} échoué :  {test} \\n')
         break
 else:
-    sortie.write("Bravo vous avez réussi tous les tests !!! \\n \\n")
-    article = js.document.querySelector("article")
-    div1 = js.document.createElement("div")
-    div1.innerHTML = """
-    <details class="help" open="">
-    <summary>Solution 1</summary>
-    <div class="highlight">
-    <pre id="__code_solution">
-    <span></span>
-    <button class="md-clipboard md-icon" title="Copier dans le presse-papier" data-clipboard-target="#__code_solution"></button>
-    def somme(tab):
-        if len(tab) > 0:
-            s = 0
-            for e in s:
-                s = s + e
-            return s
-        return None
-    </pre>
-    </details>
-    """
-    article.appendChild(div1)
-    div2 = js.document.createElement("div")
-    div2.innerHTML = """
-    <details class="help" open="">
-    <summary>Solution 2</summary>
-    <div class="highlight">
-    <pre id="__code_solution2">
-    <span></span>
-    <button class="md-clipboard md-icon" title="Copier dans le presse-papier" data-clipboard-target="#__code_solution2"></button>
-    def somme(tab):
-        if len(tab) > 0:
-            s = 0
-            for k in range(len(tab)):
-                s = s + tab[k]
-            return s
-        return None
-    </pre>
-    </details>
-    """
-    article.appendChild(div2)
-
+    sortie.write("Bravo !!!")
 sortie.getvalue()
--- docs/automatismes/tris/exo1/test_exo1_tris.py
+++ docs/automatismes/tris/exo1/test_exo1_tris.py
@@ -1,6 +1,5 @@
 import io
 import random
-import js
 
 
 def index_minimum_partiel_correction(tab:[int], debut:int)->int:
@@ -38,41 +37,5 @@ else:
             sortie.write(f"Test {i} échoué \\n")
             break
     else:        
-        sortie.write("Bravo, tests réussis pour tri_selection_croissant !!! \\n \\n")
-        sortie.write("Bravo vous avez réussi tous les tests !!! \\n \\n")
-        article = js.document.querySelector("article")
-        div1 = js.document.createElement("div")
-        div1.innerHTML = """
-        <details class="help" open="">
-        <summary>Solution <code>index_minimum_partiel</code></summary>
-        <div class="highlight">
-        <pre id="__code_solution">
-        <span></span>
-        <button class="md-clipboard md-icon" title="Copier dans le presse-papier" data-clipboard-target="#__code_solution"></button>
-        def index_minimum_partiel_correction(tab:[int], debut:int)->int:
-            imin = debut
-            for k in range(debut + 1, len(tab)):
-                if tab[k] < tab[imin]:
-                    imin = k
-            return imin
-        </pre>
-        </details>
-        """
-        article.appendChild(div1)
-        div2 = js.document.createElement("div")
-        div2.innerHTML = """
-        <details class="help" open="">
-        <summary>Solution <code>tri_selection_croissant</code></summary>
-        <div class="highlight">
-        <pre id="__code_solution2">
-        <span></span>
-        <button class="md-clipboard md-icon" title="Copier dans le presse-papier" data-clipboard-target="#__code_solution2"></button>
-        def tri_selection_croissant_correction(tab:[int])->[int]:
-            for k in range(len(tab) - 1):
-                imin = index_minimum_partiel_correction(tab, k)
-                tab[imin], tab[k] = tab[k], tab[imin]
-        </pre>
-        </details>
-        """
-        article.appendChild(div2)
+        sortie.write("Bravo, tests réussis pour tri_selection_croissant !!!")
 sortie.getvalue()
